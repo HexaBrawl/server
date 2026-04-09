aa1522e161972a19e8c39a8f3a61125165143031
Josef <j1sulzer@edu.aau.at>
2026-04-10 00:33:13 +0200

    Restore missing WebSocket endpoints after Kotlin refactor to fix integration tests

--- src/main/kotlin/at/aau/hexabrawl/websocketserver/websocket/broker/WebSocketBrokerController.kt
+++ src/main/kotlin/at/aau/hexabrawl/websocketserver/websocket/broker/WebSocketBrokerController.kt
@@ -1,29 +1,5 @@
 package at.aau.hexabrawl.websocketserver.websocket.broker
 
-/*import at.aau.hexabrawl.websocketserver.messaging.dtos.StompMessage
-import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.SendTo
-import org.springframework.stereotype.Controller
-
-@Controller
-class WebSocketBrokerController {
-
-    @MessageMapping("/hello")
-    @SendTo("/topic/hello-response")
-    fun handleHello(text: String): String {
-        // TODO handle the messages here
-        return "echo from broker: $text"
-    }
-
-    @MessageMapping("/object")
-    @SendTo("/topic/rcv-object")
-    fun handleObject(msg: StompMessage): StompMessage {
-        return msg
-    }
-}
-
-package at.aau.serg.websocketdemoserver.websocket.broker*/
-
 import at.aau.hexabrawl.websocketserver.messaging.dtos.StompMessage
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
@@ -79,6 +55,25 @@ class WebSocketBrokerController {
         }
 
         return gameState
+    }
+    // =========================
+    // Send Hello to Server
+    // =========================
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/hello-response")
+    fun handleHello(message: String): String {
+        return "echo from broker: $message"
+    }
+
+    // =========================
+    // Sends the same object back to all subscribers of /topic/rcv-object
+    // =========================
+
+    @MessageMapping("/object")
+    @SendTo("/topic/rcv-object")
+    fun handleObject(message: StompMessage): StompMessage {
+        return message
     }
 
     // =========================
