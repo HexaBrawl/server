2a8bad357378328a277cbe7f7c0ffc863a32d6ec
=SonjaBeisswaenger <=sonjabei@edu.aau.at>
2026-04-29 21:31:07 +0200

    Changed player back to String in Move class

--- src/main/kotlin/at/aau/hexabrawl/websocketserver/model/Move.kt
+++ src/main/kotlin/at/aau/hexabrawl/websocketserver/model/Move.kt
@@ -1,7 +1,7 @@
 package at.aau.hexabrawl.websocketserver.model
 
 data class Move(
-    var player: Player? = null,
+    var player: String = "",
     var type: String = "",
     var fromX: Int = 0,
     var fromY: Int = 0,
