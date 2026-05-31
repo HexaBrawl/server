067ec021b1b13fb9ae4478f6225d147fbcd08e90
=SonjaBeisswaenger <=sonjabei@edu.aau.at>
2026-05-31 18:02:07 +0200

    feat(model): add gold attribute to Player data class

--- src/main/kotlin/at/aau/hexabrawl/websocketserver/model/Player.kt
+++ src/main/kotlin/at/aau/hexabrawl/websocketserver/model/Player.kt
@@ -4,6 +4,6 @@ package at.aau.hexabrawl.websocketserver.model
 data class Player (
     val name : String = "",
     val sessionId : String = "",
-    val color: PlayerColor = PlayerColor.RED
-
+    val color: PlayerColor = PlayerColor.RED,
+    var gold: Int = 0
 ){}
