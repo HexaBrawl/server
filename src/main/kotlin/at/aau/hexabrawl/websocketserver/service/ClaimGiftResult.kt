d2e75ddb272c375f9ce28d5a13622f7c24efe271
STEFANLEGAT2002 <stefan.l2k2@gmail.com>
2026-06-23 14:43:58 +0200

    doc(service): add KDoc to sealed result types

--- src/main/kotlin/at/aau/hexabrawl/websocketserver/service/ClaimGiftResult.kt
+++ src/main/kotlin/at/aau/hexabrawl/websocketserver/service/ClaimGiftResult.kt
@@ -3,7 +3,10 @@ package at.aau.hexabrawl.websocketserver.service
 import at.aau.hexabrawl.websocketserver.model.ErrorCode
 import at.aau.hexabrawl.websocketserver.model.GameState
 
+/** Ergebnis eines [CheatGiftService.claimCheatGift]-Aufrufs. */
 sealed class ClaimGiftResult {
+    /** Geschenk wurde erfolgreich beansprucht; [state] enthält den aktualisierten Spielzustand. */
     data class Claimed(val state: GameState) : ClaimGiftResult()
+    /** Aktion wurde abgelehnt; [errorCode] und [message] beschreiben den Grund. */
     data class Rejected(val errorCode: ErrorCode, val message: String) : ClaimGiftResult()
 }
